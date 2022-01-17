9635606bc141cef588e052836098be310d0e8166
Nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-01-17 23:19:24 +0000

    Add colors

--- pc-stats.sh
+++ pc-stats.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-echo "PWD:" $(pwd)
-echo "Date:" $(date)
-echo "Up time:" $(uptime | awk '{print $2 ,$3 ,$4}')
-echo "Kernel:" $(uname -r)
-echo "Utilizador:" $(whoami)
+
+
+# Variables for colors(text)
+RED="\033[31m"
+Green="\033[0;32m"
+Yellow="\033[0;33m"
+Cyan="\033[0;36m"
+NC="\033[0m"
+
+
+
+
+
+echo -e "${Cyan}PWD:${NC}" $(pwd)
+echo "${Cyan}Date:${NC}" $(date)
+echo "${Cyan}Up time:${NC}" $(uptime | awk '{print $2 ,$3 ,$4}')
+echo "${Cyan}Kernel:${NC}" $(uname -r)
+echo "${Cyan}Utilizador:${NC}" $(whoami)
 echo " "
-echo "Memória RAM:"
+echo "${Cyan}Memória RAM:${NC}"
 echo $(free -h)
 echo " "
-echo "Espaço em Disco:"
+echo "${Cyan}Espaço em Disco:${NC}"
 echo $(df -h)
 echo " "
-echo "Dispositivos USB:"
+echo "${Cyan}Dispositivos USB:${NC}"
 echo $(lsusb)
 echo " "
 
