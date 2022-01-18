1ede7ae831faa2d0c16840125bf1e6e7502a9e4e
Nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-01-18 00:16:00 +0000

    Update pc-stats.sh

--- pc-stats.sh
+++ pc-stats.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
-
-
 # Variables for colors(text)
-RED="\033[31m"
+RED="\033[31m;"
 Green="\033[0;32m"
 Yellow="\033[0;33m"
 Cyan="\033[0;36m"
 NC="\033[0m"
-
-
-
-
-
-echo -e "${Cyan}PWD:${NC}"
-echo $(pwd)
-echo -e "${Cyan}Date:${NC}"
-echo $(date)
-echo -e "${Cyan}Up time:${NC}"
-echo $(uptime | awk '{print $2 ,$3 ,$4}')
-echo -e "${Cyan}Kernel:${NC}"
-echo $(uname -r)
-echo -e "${Cyan}Utilizador:${NC}"
-echo $(whoami)
+echo "PWD:" $(pwd)
+echo "Date:" $(date)
+echo "Up time:" $(uptime | awk '{print $2 ,$3 ,$4}')
+echo "Kernel:" $(uname -r)
+echo "Utilizador:" $(whoami)
 echo " "
 echo -e "${Cyan}Memória RAM:${NC}"
 echo $(free -h)
@@ -32,6 +20,3 @@ echo " "
 echo -e "${Cyan}Dispositivos USB:${NC}"
 echo $(lsusb)
 echo " "
-
-
-
