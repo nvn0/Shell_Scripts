8e0e99379f8e1d9e0ff8b140cc18af0c8ce5701c
Nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-01-18 00:22:04 +0000

    Update pc-stats.sh

--- pc-stats.sh
+++ pc-stats.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 # Variables for colors(text)
 RED="\033[31m;"
 Green="\033[0;32m"
 Yellow="\033[0;33m"
 Cyan="\033[0;36m"
 NC="\033[0m"
-
 echo "PWD:" $(pwd)
 echo "Date:" $(date)
 echo "Up time:" $(uptime | awk '{print $2 ,$3 ,$4}')
